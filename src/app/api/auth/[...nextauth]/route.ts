96233f004019faed219cce96a621b611ff2f361d
Vikas <vikasmaur10@gmail.com>
2023-08-15 13:28:38 +0530

    Removed export from authOptions in nextauth/route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import NextAuth, { NextAuthOptions } from 'next-auth'
 import { JWT } from 'next-auth/jwt'
 import GitHubProvider from 'next-auth/providers/github'
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
