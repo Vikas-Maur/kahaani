57262396c6a2f3801d19b09219cd12263514b815
Vikas <vikasmaur10@gmail.com>
2023-08-15 18:22:03 +0530

    Minor change made in design

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -21,7 +21,7 @@ const Home: React.FC = () => {
           alt="Story Illustration SVG"
         />
       </div>
-      <div className="fixed top-0 left-0 w-screen h-screen flex justify-center items-center lg:hidden z-10 opacity-30">
+      <div className="fixed top-0 left-0 w-screen h-screen flex lg:hidden z-10 opacity-30">
         <img
           src="/story-illustration.svg"
           className="max-w-full max-h-full block"
